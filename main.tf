b519afeb745b70f1dafab50741f079f687f42154
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-10-02 09:45:12 +0100

    added stage to create logstash resource group

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "azurerm_resource_group" "logstash-resourcegroup" {
+  name     = "${var.product}-logstash-${var.env}"
+  location = "${var.location}"
+
+  tags = "${merge(var.common_tags,
+    map("lastUpdated", "${timestamp()}")
+    )}"
+}
+
 module "logstash" {
   source = "git@github.com:hmcts/cnp-module-logstash.git?ref=master"
   product = "${var.product}"
