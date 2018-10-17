a7b5ff86bb2907deb93d868d42b6a9d5b429265e
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-10-17 15:12:23 +0100

    Add sudo ls to debug sed's

--- packer_images/install_java8.sh
+++ packer_images/install_java8.sh
@@ -6,6 +6,7 @@ sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 
 cd /var/lib/dpkg/info
+sudo ls
 sudo sed -i 's|JAVA_VERSION=8u171|JAVA_VERSION=8u181|' oracle-java8-installer.*
 sudo sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/|' oracle-java8-installer.*
 sudo sed -i 's|SHA256SUM_TGZ="b6dd2837efaaec4109b36cfbb94a774db100029f98b0d78be68c27bec0275982"|SHA256SUM_TGZ="1845567095bfbfebd42ed0d09397939796d05456290fb20a83c476ba09f991d3"|' oracle-java8-installer.*
