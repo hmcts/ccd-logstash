c3b28f66ab875c64ced691d90744d5f52c5fe70b
Isher Singh Raikmo <35302598+isherraikmo@users.noreply.github.com>
2020-10-06 13:44:42 +0100

    updated

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "azurerm_key_vault_secret" "ccd_elastic_search_public_key" {
 #}
 
 module "logstash" {
-  source = "git@github.com:hmcts/cnp-module-logstash.git?ref=no-dynatrace"
+  source = "git@github.com:hmcts/cnp-module-logstash.git?ref=master"
   product = "${var.product}"
   location = "${var.location}"
   env = "${var.env}"
