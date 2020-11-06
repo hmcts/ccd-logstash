279f2fdd155187bbbc78bf82f299360c85d47ab2
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-11-06 16:23:20 +0000

    RDM-10064: Running multiple Logstash with CCD Docker

--- bin/build-logstash-instances.sh
+++ bin/build-logstash-instances.sh
@@ -2,13 +2,13 @@
 
 docker build -t hmcts/ccd/logstash:ccd-latest --build-arg conf=ccd_logstash.conf.in .
 
-docker build -t hmcts/ccd/logstash:ccdcmc-latest --build-arg conf=ccdcmc_logstash.conf.in .
+docker build -t hmcts/ccd/logstash:cmc-latest --build-arg conf=ccdcmc_logstash.conf.in .
 
-docker build -t hmcts/ccd/logstash:ccddivorce-latest --build-arg conf=ccddivorce_logstash.conf.in .
+docker build -t hmcts/ccd/logstash:divorce-latest --build-arg conf=ccddivorce_logstash.conf.in .
 
-docker build -t hmcts/ccd/logstash:ccdethos-latest --build-arg conf=ccdethos_logstash.conf.in .
+docker build -t hmcts/ccd/logstash:ethos-latest --build-arg conf=ccdethos_logstash.conf.in .
 
-docker build -t hmcts/ccd/logstash:ccdprobate-latest --build-arg conf=ccdprobate_logstash.conf.in .
+docker build -t hmcts/ccd/logstash:probate-latest --build-arg conf=ccdprobate_logstash.conf.in .
 
-docker build -t hmcts/ccd/logstash:ccdsscs-latest --build-arg conf=ccdsscs_logstash.conf.in .
+docker build -t hmcts/ccd/logstash:sscs-latest --build-arg conf=ccdsscs_logstash.conf.in .
 
