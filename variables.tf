2504a8e519bb20979f2dd8022c585edfb5ee7ae5
Ben Neill <benjaminrneill@gmail.com>
2021-01-11 23:29:38 +0000

    comment out broken DT vars

--- variables.tf
+++ variables.tf
@@ -42,11 +42,11 @@ variable "name" {
   type = "string"
 }
 
-variable "dynatrace_instance" {}
+#variable "dynatrace_instance" {}
 
-variable "dynatrace_token" {}
+#variable "dynatrace_token" {}
 
-variable "dynatrace_hostgroup" {}
+#variable "dynatrace_hostgroup" {}
 
 variable "mgmtprod_subscription_id" {
   default = "8999dec3-0104-4a27-94ee-6588559729d1"
